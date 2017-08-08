80b9fd8785a05a8e97ccce2f30e7bc530a20aec0
Justin Eder <justineder@gmail.com>
2017-08-08 15:58:17 -0500

    clean directories

---
+++ README.md
@@ -0,0 +1,28 @@
+# mozjpeg-compressor
+
+Reads a directory of images and compresses all `.jpeg` and `.jpg` files.
+
+## Instructions for use
+
+* Open a terminal to first clone this repository
+* Drop all files you want to compress into the `LARGE_IMAGES` directory
+* In the terminal, cd into the `mozjpeg-compressor` directory
+* In the terminal: run `npm install` to install package dependencies (only need to do this once on initial install)
+* To start the mozjpeg-compressor, run `npm start` in the terminal
+* Access your minified files in `MINIFIED_IMAGES` 
+
+### Prerequisites
+
+Node must be installed on your machine. To find out if Node.js is installed, open a terminal window and run `node -v`. You'll see a version number printed to the console if Node is installed on your system.
+
+## Built With
+
+* [mozjpeg](https://www.npmjs.com/package/mozjpeg) - The mozjpeg compression engine
+
+## Authors
+
+* **Justin Eder** - *EMC School* - [JstnEdr](https://github.com/JstnEdr)
+
+## License
+
+This project is licensed under the MIT License
---
+++ main.js
@@ -0,0 +1,42 @@
+var execFile = require('child_process').execFile;
+var mozjpeg = require('mozjpeg');
+var fs = require('fs');
+
+// GET
+var inputDir = "./LARGE_IMAGES";
+var outputDir = "./MINIFIED_IMAGES";
+var jpegArray = [];
+
+// ===== BUILD THE JPEG ARRAY ===== //
+fs.readdir(inputDir, function(err, items) {
+    for (var i=0; i<items.length; i++) {
+      let item = items[i];
+      let index = item.indexOf('.');
+      let ext = item.slice(index);
+      if (ext === '.jpg' || ext === '.jpeg') {
+        jpegArray.push(item);
+      }
+    }
+    // console.log(jpegArray.length);
+    compressImagesInArray(jpegArray)
+});
+
+console.log('Directories: ', inputDir, outputDir);
+
+// ===== COMPRESS THE IMAGES ===== //
+function compressImagesInArray(arrayToCompress) {
+  let pos = 0;
+  function compressSingleImage(image) {
+    if (pos < arrayToCompress.length) {
+      let inputFile = inputDir + '/' + image;
+      let outputFile = outputDir + '/' + image;
+      // console.log('FILES', inputFile, outputFile);
+      execFile(mozjpeg, ['-outfile', outputFile, inputFile], function (err) {
+          console.log('Image minified:', outputFile);
+          pos++;
+          compressSingleImage(arrayToCompress[pos])
+      });
+    }
+  }
+  compressSingleImage(arrayToCompress[pos]);
+}
